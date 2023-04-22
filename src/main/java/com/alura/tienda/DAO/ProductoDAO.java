9626c04a5c5972e0557a65b980d45aab52b77f09
jeassons <jeassonsuarez@outlook.es>
2023-04-22 13:05:19 -0500

    añadiendo consultas con filtros

--- src/main/java/com/alura/tienda/DAO/ProductoDAO.java
+++ src/main/java/com/alura/tienda/DAO/ProductoDAO.java
@@ -1,8 +1,12 @@
 package com.alura.tienda.DAO;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import com.alura.tienda.modelo.Producto;
+
+import net.bytebuddy.asm.Advice.Return;
 
 public class ProductoDAO {
 	private EntityManager em;
@@ -13,6 +17,20 @@ public class ProductoDAO {
 	
 	public void guardar(Producto producto) {
 		this.em.persist(producto);
+	}
+	
+	public Producto consultaId(Long id) {
+		return em.find(Producto.class, id);
+	}
+	
+	public List<Producto> consultarTodos(){
+		String jpql = "SELECT P FROM Producto AS P";
+		return em.createQuery(jpql, Producto.class).getResultList();
+	}
+	
+	public List<Producto> consuPorNombre(String nombre){
+		String jpql = "SELECT P FROM Producto AS P WHERE P.nombre=:nombre";
+		return em.createQuery(jpql, Producto.class).setParameter("nombre", nombre).getResultList();
 	}
 	
 }
--- src/main/java/com/alura/tienda/pruebas/RegistroDeProducto.java
+++ src/main/java/com/alura/tienda/pruebas/RegistroDeProducto.java
@@ -1,6 +1,7 @@
 package com.alura.tienda.pruebas;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,6 +18,65 @@ public class RegistroDeProducto {
 	public static void main(String[] args) {
 		
 		
+		registrarProducto();
+		
+		EntityManager em = JPAUtils.getEntityManager();		
+		ProductoDAO productoDAO = new ProductoDAO(em);
+
+		Producto producto = productoDAO.consultaId(7l);
+		
+		List<Producto> productos = productoDAO.consultarTodos();
+		
+		System.out.println(producto.getNombre());
+		
+		for (Producto p : productos) {
+			System.out.println(p.getNombre());
+		}
+	
+	
+		List<Producto> p = productoDAO.consuPorNombre("Samsung");
+		
+		for (Producto producto2 : p) {
+			System.out.println(producto2.getDescripcion());
+		}
+		
+		
+		
+		
+		
+		
+		
+////		se utilizara para dar persistencia en la base de datos, ademas permite hacer rollback
+//		em.flush();
+//		
+////		aguardar la conexxion, pasa a un estado detach
+//		em.clear();
+//		
+////		EN CASO DE QUERER REALIZAR UN UPDATE DE UN REGISTRO, ES POSIBLE REALIZARLO USANDO EL METODO DE ENTITY LLAMADO MERGER,
+////		EL CUAL RECIVE COMO PARAMETRO LA ENTIDAD, POR EJEMPLO EN ESTE CASO SE PASARA EL CELULARES YA QUE ES LA CATEGORIA
+//		
+//		celulares = em.merge(celulares);
+//		
+//		celulares.setNombre("SOFTWARE");
+//		
+////		entidad con estado managed
+//		em.flush();
+//		
+////		entidad entra a esado detach
+//		em.clear();
+//		
+////		entidad vuelve al estado managed
+//		celulares=em.merge(celulares);
+//		
+//		em.remove(celulares);
+//		em.flush();
+		
+		
+		
+		
+	}
+
+	private static void registrarProducto() {
 		Categoria celulares = new Categoria("Celulares");
 		
 		
@@ -26,7 +86,7 @@ public class RegistroDeProducto {
 		EntityManager em = JPAUtils.getEntityManager();		
 		
 //		metodos de conexion con la base de datos
-//		ProductoDAO productoDAO = new ProductoDAO(em);
+		ProductoDAO productoDAO = new ProductoDAO(em);
 		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
 
@@ -35,35 +95,10 @@ public class RegistroDeProducto {
 		
 //		se realiza la operacion de persistencia en la base de datos
 		categoriaDAO.guardar(celulares);
-//		productoDAO.guardar(celular);
+		productoDAO.guardar(celular);
 		
-//		se utilizara para dar persistencia en la base de datos, ademas permite hacer rollback
-		em.flush();
-		
-//		aguardar la conexxion, pasa a un estado detach
-		em.clear();
-		
-//		EN CASO DE QUERER REALIZAR UN UPDATE DE UN REGISTRO, ES POSIBLE REALIZARLO USANDO EL METODO DE ENTITY LLAMADO MERGER,
-//		EL CUAL RECIVE COMO PARAMETRO LA ENTIDAD, POR EJEMPLO EN ESTE CASO SE PASARA EL CELULARES YA QUE ES LA CATEGORIA
-		
-		celulares = em.merge(celulares);
-		
-		celulares.setNombre("SOFTWARE");
-		
-//		entidad con estado managed
-		em.flush();
-		
-//		entidad entra a esado detach
-		em.clear();
-		
-//		entidad vuelve al estado managed
-		celulares=em.merge(celulares);
-		
-		em.remove(celulares);
-		em.flush();
-		
-		
-		
-		
+//		
+		em.getTransaction().commit();
+		em.close();
 	}
 }
